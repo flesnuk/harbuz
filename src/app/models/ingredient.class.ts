06869e681df50f7d4bb529f1a4e743817365de32
flesnuk <flesnuk@gmail.com>
2017-07-01 22:00:18 +0200

    tslint

--- src/app/models/ingredient.class.ts
+++ src/app/models/ingredient.class.ts
@@ -13,7 +13,7 @@ export class Ingredient {
 export class IngredientPrice extends Ingredient {
 	price: EuroUnit
 
-	constructor( name: string, price?: EuroUnit){
+	constructor( name: string, price?: EuroUnit) {
 		price = price == null ? new EuroMassUnit(null, MassUnit.mg) : price;
 		super(name, price.Unit);
 		this.price = price;
@@ -23,13 +23,13 @@ export class IngredientPrice extends Ingredient {
 export class IngredientQuantity extends Ingredient {
 	quantity: UnitsOfMeasurement
 
-	constructor(name: string, quantity: UnitsOfMeasurement){
+	constructor(name: string, quantity: UnitsOfMeasurement) {
 		super(name, quantity.Unit);
 		this.quantity = quantity;
 	}
 }
 
-/** 
+/**
 *	Euro with a stablished unit (euro/unit)
 */
 abstract class EuroUnit {
@@ -37,22 +37,22 @@ abstract class EuroUnit {
 	unit: MassUnit | VolumeUnit
 	Unit: typeof MassUnit | typeof VolumeUnit
 
-	constructor(price: Euro, Unit: typeof MassUnit | typeof VolumeUnit ,unit: MassUnit |  VolumeUnit) {
+	constructor(price: Euro, Unit: typeof MassUnit | typeof VolumeUnit, unit: MassUnit |  VolumeUnit) {
 		this.price = price == null ? new Euro() : price;
 		this.Unit = Unit;
 		this.unit = unit;
 	}
 
-	mul(physicalProp: UnitsOfMeasurement) : Euro{
+	mul(physicalProp: UnitsOfMeasurement): Euro {
 		return this.price.mul( physicalProp.value * (physicalProp.unit / this.unit) );
 	}
 
 	toString () {
-		return this.price.toString() + "/" + this.Unit[this.unit];
+		return this.price.toString() + '/' + this.Unit[this.unit];
 	}
 }
 
-/** 
+/**
 *	Euro with a mass unit (euro/mass)
 */
 export class EuroMassUnit extends  EuroUnit {
@@ -63,7 +63,7 @@ export class EuroMassUnit extends  EuroUnit {
 
 }
 
-/** 
+/**
 *	Euro with a volume unit (euro/volume)
 */
 export class EuroVolumeUnit extends  EuroUnit {
@@ -92,9 +92,9 @@ abstract class UnitsOfMeasurement {
 	unit: MassUnit | VolumeUnit
 	value: number
 
-	constructor(Unit: typeof MassUnit | typeof VolumeUnit, 
+	constructor(Unit: typeof MassUnit | typeof VolumeUnit,
 				value?: number
-			){
+			) {
 		this.Unit = Unit;
 		this.value = value | 0;
 	}
@@ -106,7 +106,7 @@ abstract class UnitsOfMeasurement {
 
 export class Mass extends UnitsOfMeasurement {
 
-	constructor(value? : number, unit? : MassUnit ) {
+	constructor(value?: number, unit?: MassUnit ) {
 		super(MassUnit, value);
 		this.unit = unit == null ? MassUnit.mg : unit;
 	}
@@ -115,7 +115,7 @@ export class Mass extends UnitsOfMeasurement {
 
 export class Volume extends UnitsOfMeasurement {
 
-	constructor(value? : number, unit? : VolumeUnit ) {
+	constructor(value?: number, unit?: VolumeUnit ) {
 		super(VolumeUnit, value);
 		this.unit = unit == null ? VolumeUnit.mL : unit;
 	}
@@ -128,48 +128,48 @@ const CENTS_IN_EURO = 100;
 export class Euro {
 	price: number
 	cents: number
-	precision: number = 2
+	precision = 2
 
 	formatter = new Intl.NumberFormat('es-ES', {
-  		style: 'currency',
-  		currency: 'EUR',
-  		minimumFractionDigits: this.precision,
+		style: 'currency',
+		currency: 'EUR',
+		minimumFractionDigits: this.precision,
 	});
 
-	constructor (price: string = "0", centsOpt?: number) {
+	constructor (price: string = '0', centsOpt?: number) {
 		if (centsOpt != null) {
 			this.cents = centsOpt;
 			return;
 		}
 
-		price = price.replace(',', '.').replace(/[^\d.-]/g,''); // price as	"4.1234" / ".25" / 		"1." 
-		let priceFloat = isNaN(parseFloat(price)) ? 0 : parseFloat(price); //4.1234 /  0.25 / 		 1 
-		let priceString = priceFloat.toFixed(this.precision); // eg: 					"4.12" ,  "0.25",	 	"1.00"
-		
-		let [euro, cents] = priceString.split("."); // eg:				["4", "12"], ["0","25"],   ["1", "00"]
-		this.cents =  parseInt(euro) * CENTS_IN_EURO;
-		this.cents += parseInt(cents);
-		
+		price = price.replace(',', '.').replace(/[^\d.-]/g, ''); // price as	"4.1234" / ".25" / 		"1."
+		const priceFloat = isNaN(parseFloat(price)) ? 0 : parseFloat(price); // 4.1234 /  0.25 / 		 1
+		const priceString = priceFloat.toFixed(this.precision); // eg: 					"4.12" ,  "0.25",	 	"1.00"
+
+		const [euro, cents] = priceString.split('.'); // eg:				["4", "12"], ["0","25"],   ["1", "00"]
+		this.cents =  parseInt(euro, 10) * CENTS_IN_EURO;
+		this.cents += parseInt(cents, 10);
+
 	}
 
-	setPrice(priceString: string = "0"){
+	setPrice(priceString: string = '0') {
 		this.constructor(priceString);
 	}
 
-	getPrice() : string{
+	getPrice(): string {
 		return (this.cents / 100 ).toString();
 	}
 
-	sum(other: Euro) : Euro {
-		return new Euro("", this.cents + other.cents);
+	sum(other: Euro): Euro {
+		return new Euro( '', this.cents + other.cents);
 	}
 
-	sub(other: Euro) : Euro {
-		return new Euro("", this.cents - other.cents);	
+	sub(other: Euro): Euro {
+		return new Euro('', this.cents - other.cents);
 	}
 
-	mul(mult: number) : Euro{
-		return new Euro("", this.cents * mult);
+	mul(mult: number): Euro {
+		return new Euro('', this.cents * mult);
 	}
 
 	toString() {
